d1a2ae704fb03dfcce4d5ee82a5c5c70d5e85e39
Himanshu4732 <himanshukumar9507638964@gmail.com>
2024-07-09 11:54:16 +0530

    read and write files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,7 +11,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 
 app.get('/',function(req,res){
-    res.render("index");
+    fs.readdir(`./files`, function(err,files){
+        if(err) console.log(err);
+        res.render("index", {files: files});
 })
+})
+
+app.get('/create',function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(" ").join('')}.txt`, req.body.detail, function(err){
+        res.redirect('/');
+    })
+
+    })
 
 app.listen(3000)
